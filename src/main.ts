a8988ec7ac23b629d285d3be0f088192b76c6a3d
Kelian Baert <kelian@baert.me>
2021-06-22 08:34:18 +0200

    changes & clean-up for express version

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,14 @@
-import "./main.css";
+import express from "express";
 
-console.log("Hello there");
+const app = express();
+const port = 8002; // default port to listen on
+
+// Define a route handler for the default home page
+app.get("/", (req, res) => {
+    res.send("Hello world!");
+});
+
+// Start the Express server
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
+});
