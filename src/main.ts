b3fa737d4931a998bc380dcdd02f02af567e3912
Kelian Baert <kelian.baert@mikrosimage.com>
2023-12-04 10:06:14 +0100

    changes for vanilla branch

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,1 @@
-import express from "express";
-
-const app = express();
-const port = 8002; // default port to listen on
-
-// Define a route handler for the default home page
-app.get("/", (req, res) => {
-    res.send("Hello world!");
-});
-
-// Start the Express server
-app.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}`);
-});
+console.log("Hello world!");
